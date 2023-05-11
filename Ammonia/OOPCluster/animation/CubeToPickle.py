be95377f450280b317cbf82369608f52e9df55fc
Vít Turčin <77553864+turcinv@users.noreply.github.com>
2023-05-11 09:47:37 +0200

    Add files via upload

--- Ammonia/OOPCluster/animation/CubeToPickle.py
+++ Ammonia/OOPCluster/animation/CubeToPickle.py
@@ -3,6 +3,13 @@ from VMDLite import read_cube_file
 
 
 def cube_to_pickle(frame):
+    """
+    Save data from cube file to pickle binary file
+    :param frame: number of frame
+    :return: pickle file
+    """
+
+    # loading cube file
     origin, step_size, data, atoms = read_cube_file(f"./cube_files/small-clusters-SPIN_DENSITY-1_{frame}_small.cube")
     info = {"origin": origin,
             "step_size": step_size,
@@ -10,5 +17,6 @@ def cube_to_pickle(frame):
             "atoms": atoms,
             }
 
+    # saving data to pickle file
     with open(f'./cube_files_pickle/ammonia-{frame}.pickle', 'wb') as file:
         pickle.dump(info, file, -1)
